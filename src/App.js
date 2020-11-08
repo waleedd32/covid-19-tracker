9a4d2a18871dcd4ac7256cbfeebc26371b1c471f
waleedd32 <valid0@hotmail.com>
2020-11-08 20:31:36 +0200

    This is just testing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Lets build Coronairus tracker !</h1>
+      <h2>This is a test...</h2>
+    </div>
+  );
+}
+export default App;
