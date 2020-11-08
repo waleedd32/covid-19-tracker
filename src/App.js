756e68ca0419acbe4cb60a7ba80ca1191dbe724a
waleedd32 <valid0@hotmail.com>
2020-11-08 20:56:21 +0200

    installed material ui dependencies and made test dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { MenuItem, FormControl, Select } from "@material-ui/core"
 import './App.css';
 
 function App() {
 
   return (
     <div className="app">
-      <h1>Lets build Coronairus tracker !</h1>
-      <h2>This is a test...</h2>
+      <h2>Lets build Coronairus tracker !</h2>
+      <FormControl className="app__dropdown" >
+        <Select variant="outlined"
+          value="cde" >
+          <MenuItem value="worldwide">Worldwide </MenuItem>
+          <MenuItem value="worldwide">chosen 1 </MenuItem>
+          <MenuItem value="worldwide">chosen 2 </MenuItem>
+          <MenuItem value="worldwide">Yeees </MenuItem>
+
+        </Select>
+
+      </FormControl>
+      {/* Header */}
+      {/* Title + select input dropdown field */}
+      {/* InfoBoxs these contain stats*/}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* Table (list of countries and cases) */}
+      {/* Graph */}
+      {/* Map */}
+
     </div>
   );
 }
