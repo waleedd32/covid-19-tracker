ed2a2de4d224fde738d64bd76aa434ec25c6f99d
waleedd32 <valid0@hotmail.com>
2023-08-14 17:28:21 +0300

    Add tests for InfoBox component

---
+++ src/__tests__/InfoBox.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import InfoBox from "../InfoBox";
+
+afterEach(cleanup); // Clean up on exiting
+
+describe("<InfoBox />", () => {
+  it("renders the InfoBox component with provided props", () => {
+    const props = {
+      title: "Cases",
+      cases: "100",
+      total: "500",
+      isRed: true,
+      active: true,
+    };
+
+    const { getByText } = render(<InfoBox {...props} />);
+
+    // Check if title, cases, and total are rendered correctly
+    expect(getByText("Cases")).toBeInTheDocument();
+    expect(getByText("100")).toBeInTheDocument();
+    expect(getByText("500 Total")).toBeInTheDocument();
+  });
+
+  it('applies the "infoBox--red" class when isRed is true', () => {
+    const { container } = render(<InfoBox isRed={true} />);
+
+    expect(container.firstChild).toHaveClass("infoBox--red");
+  });
+
+  it('applies the "infoBox--selected" class when active is true', () => {
+    const { container, debug } = render(<InfoBox active={true} />);
+    debug();
+
+    expect(container.firstChild).toHaveClass("infoBox--selected");
+  });
+
+  it('applies the "infoBox--selectedtored" class when activetored is true', () => {
+    const { container } = render(<InfoBox activetored={true} />);
+    expect(container.firstChild).toHaveClass("infoBox--selectedtored");
+  });
+});
