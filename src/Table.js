99906735da71a93d2670db52b6e6ba431ced0142
waleedd32 <valid0@hotmail.com>
2023-08-23 17:21:38 +0300

    Refactored Table component to use <table> tag and updated CSS styles

--- src/Table.js
+++ src/Table.js
@@ -5,24 +5,27 @@ import numeral from "numeral";
 function Table({ countries }) {
   return (
     <div className="table">
-      {/* here we have done destructuring(split it a part) */}
-      {countries.map(({ country, cases, countryInfo }) => (
-        <tr>
-          <td>
-            <div>
-              {" "}
-              <img
-                src={countryInfo.flag}
-                style={{ height: "23px", width: "35px" }}
-              />
-            </div>
-          </td>
-          <td>{country} </td>
-          <td>
-            <strong> {numeral(cases).format("0,0")} </strong>
-          </td>{" "}
-        </tr>
-      ))}
+      <table>
+        <tbody>
+          {/* here we have done destructuring(split it a part) */}
+          {countries.map(({ country, cases, countryInfo }) => (
+            <tr key={country}>
+              <td>
+                <div>
+                  <img
+                    src={countryInfo.flag}
+                    style={{ height: "23px", width: "35px" }}
+                  />
+                </div>
+              </td>
+              <td>{country}</td>
+              <td>
+                <strong>{numeral(cases).format("0,0")}</strong>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
